acde25301ca03bd5d582e5e9d50b0e3ee2f1758f
Ihor Sokolyk <ihorsokolyk1993@gmail.com>
2018-05-25 15:40:20 +0300

    first commit

---
+++ src/main/java/com/springboot/profiles/Application.java
@@ -0,0 +1,14 @@
+package com.springboot.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setAdditionalProfiles("dev");
+        application.run(args);
+    }
+}
---
+++ src/main/java/com/springboot/profiles/config/Config.java
@@ -0,0 +1,7 @@
+package com.springboot.profiles.config;
+
+public interface Config {
+
+    void setup();
+    
+}
